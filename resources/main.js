ac0bbdbd2e57382e0944a75850b49a23a517998a
ecruz22 <erickf22@rocketmail.com>
2020-01-14 12:08:54 -0500

    Update main.js

--- resources/main.js
+++ resources/main.js
@@ -1,6 +1,14 @@
 function pregunta(){ 
-    if (confirm('¿Está seguro que desea enviar esta información?')){ 
+    var mensaje;
+    var opcion = confirm('¿Está seguro que desea enviar esta información?');
+    if (opcion == true) {
+        mensaje = "Mensaje enviado";
+	} else {
+	    mensaje = "Mensaje no enviado";
+	}
+	//document.getElementById("ejemplo").innerHTML = mensaje;
+    
+    /*if (confirm('¿Está seguro que desea enviar esta información?')){ 
        document.formulario.submit();
-    } 
-} 
-//alert();
+    } */
+}
